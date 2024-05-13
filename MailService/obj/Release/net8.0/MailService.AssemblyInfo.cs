ded34b50368ada48f5acc823307e104f529a55ca
Wufferman <eaajbst@students.eaaa.dk>
2024-05-13 12:17:06 +0200

    Hmmm

--- MailService/obj/Release/net8.0/MailService.AssemblyInfo.cs
+++ MailService/obj/Release/net8.0/MailService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MailService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1878a6ff3fc7556ed7cc05d017428511392983a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555fe07078f41f0a089eb47ed7ee29ee85eac3fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MailService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MailService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
