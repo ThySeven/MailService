2f3f2b58e8df17020ce5bcb8a2703dd417c38733
Orluff <nicolaj.orluff@gmail.com>
2024-05-15 08:26:22 +0200

    Ryddet op i kode

--- MailService/Services/MailReceiver.cs
+++ MailService/Services/MailReceiver.cs
@@ -16,7 +16,7 @@ public class EmailReceiver
 
     public EmailReceiver()
     {
-        
+
     }
 
     public void ListenAndSendToRabbitMQ()
@@ -94,14 +94,14 @@ public class EmailReceiver
             var serializedEmail = JsonSerializer.Serialize(mail);
 
             // Publish the serialized email data to RabbitMQ
-            channel.QueueDeclare(queue: "MailQueue",
+            channel.QueueDeclare(queue: Environment.GetEnvironmentVariable("RabbitMQQueueName"),
                                           durable: false,
                                           exclusive: false,
                                           autoDelete: false,
                                           arguments: null);
 
             channel.BasicPublish(exchange: "",
-                                          routingKey: "MailQueue",
+                                          routingKey: Environment.GetEnvironmentVariable("RabbitMQQueueName"),
                                           basicProperties: null,
                                           body: Encoding.UTF8.GetBytes(serializedEmail));
 
